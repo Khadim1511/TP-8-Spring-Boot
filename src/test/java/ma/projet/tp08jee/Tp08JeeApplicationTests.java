ed0701406fc28612adc6d45b15a7b280e1f2eae7
MAC BOOK <khadimseye1511@gmail.com>
2026-03-15 23:50:59 +0000

    Premier commit

---
+++ src/test/java/ma/projet/tp08jee/Tp08JeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.projet.tp08jee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp08JeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
